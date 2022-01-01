a8da6dfaa394048e8f2bdf80bd55b6e18c64e5fa
Amol Galande <amoloct90@gmail.com>
2022-01-01 12:37:39 -0600

    added config parameters for fpn_resnet

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -23,8 +23,8 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 # model-related
-from tools.objdet_models.resnet.models import fpn_resnet
-from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.fpn_resnet.models import fpn_resnet
+from tools.objdet_models.fpn_resnet.utils.evaluation_utils import decode, post_processing 
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
@@ -61,6 +61,51 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'fpn_resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.pretrained_path = 'tools/objdet_models/fpn_resnet/models/pretrained/fpn_resnet_18_epoch_300.pth'
+        configs.saved_fn = 'fpn_resnet'
+        configs.arch = 'fpn_resnet'
+        configs.K = 50
+
+        configs.num_samples = None
+        configs.num_workers = 1
+        configs.batch_size = 4
+        configs.peak_thresh = 0.5
+        configs.save_test_output = False
+        configs.output_formate = 'image'
+        configs.output_video_fn = 'out_fpn_resnet_18'
+        configs.output_width = 608
+        configs.distributed = False
+        configs.img_size = 608
+        configs.nms_thresh = 0.4
+        configs.num_samples = None
+        configs.num_workers = 4
+        
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
+
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+            }
+        configs.num_input_features = 4
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -118,7 +163,7 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
-
+        model = fpn_resnet()
         #######
         ####### ID_S3_EX1-4 END #######     
     
