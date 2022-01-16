a9a20af4692e3b7d282a051e7795e474a5cbb012
Amol Galande <amoloct90@gmail.com>
2022-01-16 14:59:19 -0600

    loop updates

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -52,15 +52,20 @@ import misc.params as params
 
 ## Select Waymo Open Dataset file and frame numbers
 data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
-# data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
+#data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 #data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [150, 151] # show only frames in interval for debugging
+show_only_frames = [0, 200] # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', data_filename) # adjustable path in case this script is called from another working directory
-model = "fpn_resnet"
-sequence = "1"
-results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results\\'+ model + '\\results_sequence_' + sequence + '_' + model)
+
+# Uncomment lines  63, 64, 65 to run object detection from lidar pcl 
+# model = "fpn_resnet" # options 'fpn_resnet', 'darknet'
+# sequence = "1" # options '1' , '2' , '3'
+# results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results\\'+ model + '\\results_sequence_' + sequence + '_' + model) 
+
+# Result file path for multi-tracking
+results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results\\'+ 'Lidar_Detections_Tracking_Final_Project')
 datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
@@ -71,10 +76,10 @@ model_det = det.create_model(configs_det)
 configs_det.use_labels_as_objects = False # True = use groundtruth labels as objects, False = use model-based detection
 
 ## Uncomment this setting to restrict the y-range in the final project
-# configs_det.lim_y = [-25, 25] 
+configs_det.lim_y = [-25, 25] 
 
 ## Initialize tracking
-KF = Filter() # set up Kalman filter 
+KF = Filter() # set up Kalman filter
 association = Association() # init data association
 manager = Trackmanagement() # init track manager
 lidar = None # init lidar sensor object
@@ -83,9 +88,9 @@ np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
 exec_data = [] # options are 'pcl_from_rangeimage', 'load_image'
-exec_detection = ['bev_from_pcl', 'detect_objects'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
-exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = ['show_objects_in_bev_labels_in_camera'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_detection = [] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+exec_tracking = ['perform_tracking'] # options are 'perform_tracking'
+exec_visualization = ['show_tracks'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0 # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
